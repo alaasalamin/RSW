2b625880c0643584f1666964e02b77e1d49c5449
alaasalamin96 <alaasalamin1996@icloud.com>
2025-11-09 21:39:20 +0100

    Add Tenant model and configure tenancy package

---
+++ app/Models/Tenant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant
+{
+    protected $fillable = [
+        'id',
+        'name',
+        'domain',
+    ];
+}
